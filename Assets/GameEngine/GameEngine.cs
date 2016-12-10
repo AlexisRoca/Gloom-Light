4da2115d28afef935213325117790829174c00c6
PC-FIXE-LUCAS\fouca <foucault.lucas@gmail.com>
2016-12-10 14:48:11 +0100

    [CONTROL] Add controller mouvement

--- Assets/GameEngine/GameEngine.cs
+++ Assets/GameEngine/GameEngine.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameEngine : MonoBehaviour
 {
+    public Player m_player;
+    public Pad m_pad;
+
     private void Awake()
     {
         // Get all elements from the scene
@@ -11,12 +14,12 @@ public class GameEngine : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-	
-	}
+        m_pad    = new Pad();
+        m_player = new Player(m_pad);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+    }
 }
