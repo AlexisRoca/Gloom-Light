bb8fe51bb3d69cb42a418c3b5713560e646e6de1
Goudé <goude.ific@outlook.sa>
2016-12-10 19:34:50 +0100

    [SOUNDS] Fridge sounds

--- Assets/Scripts/Objects/Abstract Class/AbstractObject.cs
+++ Assets/Scripts/Objects/Abstract Class/AbstractObject.cs
@@ -4,8 +4,20 @@ using System.Collections;
 abstract public class AbstractObjects : MonoBehaviour
 {
 
-    public Animator m_animator;
-    public AudioSource m_audioSource;
+    protected Animator m_animator;
+    protected AudioSource m_audioSource;
+
+    // Use this for initialization
+    void Start() {
+        // Animator
+        if (this.gameObject.GetComponent<Animator>() != null)
+            m_animator = this.gameObject.GetComponent<Animator>();
+
+        // Audio
+        if (this.gameObject.GetComponent<AudioSource>() != null)
+            m_audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
 
     // Play animation and sound
     abstract protected void Play();
