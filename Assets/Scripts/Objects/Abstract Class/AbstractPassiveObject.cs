5dd4d5daad27708b5d19e1602f06fdcc7da6d3b4
Goudé <goude.ific@outlook.sa>
2016-12-10 17:25:08 +0100

    [OBJECTS] Objects interractions works (fridge example done)

--- Assets/Scripts/Objects/Abstract Class/AbstractPassiveObject.cs
+++ Assets/Scripts/Objects/Abstract Class/AbstractPassiveObject.cs
@@ -4,7 +4,7 @@ using System.Collections;
 abstract public class AbstractPassiveObject : AbstractObjects {
 
     // Event calls when collision
-    void OnCollisionEnter(Collision collision) {
+    void OnTriggerEnter(Collision collision) {
         if (collision.gameObject.GetComponent<Player>() != null) {
             Play();
         }
