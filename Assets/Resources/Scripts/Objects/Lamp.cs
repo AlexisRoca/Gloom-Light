ae3e450ec3701bec5697d010e50227c2e4a1d2d9
Vincent Goupil <goupil.vince@gmail.com>
2016-12-12 16:59:05 +0100

    [Main Scene] Arranging

--- Assets/Resources/Scripts/Objects/Lamp.cs
+++ Assets/Resources/Scripts/Objects/Lamp.cs
@@ -7,12 +7,13 @@ public class Lamp : AbstractActiveObject
     // Attributes
     bool m_isOn = false;
     Light m_light;
+    //Renderer m_materialLights;
 
     // Use this for initialization
     void Start()
     {
         m_light = this.gameObject.GetComponentInChildren<Light>();
-
+        //m_materialLights = GetComponentInChildren<Renderer>();
         // Object disable
         m_isOn = false;
         m_light.enabled = false;
@@ -24,13 +25,13 @@ public class Lamp : AbstractActiveObject
         if (m_isOn)
         {
             m_light.enabled = false;
-
+            //m_materialLights.material.SetColor("_EmissionColor", Color.white);
             m_isOn = false;
         }
         else
         {
             m_light.enabled = true;
-
+            //m_materialLights.material.SetColor("_EmissionColor", Color.black);
             m_isOn = true;
         }
     }
